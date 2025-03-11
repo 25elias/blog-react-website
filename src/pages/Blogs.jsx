7e76d12ee97731aba15436119302c0b0f5f34f01
25elias <eliasusman83@gmail.com>
2025-03-11 13:43:19 +0300

    blog page

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -4,7 +4,7 @@ import BlogPage from '../components/BlogPage'
 const Blogs = () => {
   return (
     <div className="">
-        <div className='bg-black text-white px-8 py-32 m-auto text-center'>
+        <div className=' bg-slate-300 text-black px-8 py-32 m-auto text-center'>
           <h1 className='text-5xl lg:text-7xl leading-[1.5] font-semibold py-8'>Blog Page</h1>
         </div>
         <div className='max-w-7xl m-auto h-full '>
